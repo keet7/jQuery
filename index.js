8fc13a9aa0c74d35332e03ae596522a8c2e0b282
keet7 <keerthana.pepeti@gmail.com>
2023-10-11 16:23:30 +0530

    manipulating the text using jQuery

--- index.js
+++ index.js
@@ -7,7 +7,15 @@
 //using the jQuery
 $("h1").css("color", "green");
 console.log($("h1").css("font-size"));
-$("button");*/
+$("button");
 
 $("h1").addClass("big-title margin-50");
 //$("h1").removeClass("big-title");
+
+*/
+
+$("h1").text("Bye");
+
+$("button").text("Don't Click Me");
+
+$("button").html("<em>Hey</em>");
