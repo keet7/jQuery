a2db5a7e8319de45f8d5efdd64f63ba335f19981
keet7 <keerthana.pepeti@gmail.com>
2023-10-11 16:17:03 +0530

    add and remove class using jQuery

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $("h1").css("color", "red");
 })
+
+//document.querySelector("h1");
+
+//using the jQuery
+$("h1").css("color", "green");
+console.log($("h1").css("font-size"));
+$("button");*/
+
+$("h1").addClass("big-title margin-50");
+//$("h1").removeClass("big-title");
